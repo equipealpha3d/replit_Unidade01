0372ae23cef108a4735094dc45f4a93049987df5
geracao3d <26634340+ALPHA3DCLASS@users.noreply.github.com>
2021-07-20 14:46:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import time
+# Print out
+print("Part 1 demonstration: printing")
+time.sleep(1.3)
+print("Output: Hello world")
+time.sleep(1.3)
+# Variables
+print("Part 2 demonstration: variables")
+time.sleep(1.3)
+
+name = "Bob"
+print("Output: Hi",name)
+time.sleep(1.3)
+
+# F String
+print("Part 3 demo: F string ")
+time.sleep(1.3)
+
+name = "Bob"
+print(f"Output: Hello, {name}")
